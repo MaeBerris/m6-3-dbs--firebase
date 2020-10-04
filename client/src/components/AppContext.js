71e993a44d34f445e78c1ce464efd230dce85a8e
Mae <maeberris@outlook.com>
2020-10-04 16:29:12 -0400

    completed exercise 1

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,6 +1,16 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext(null);
+
+var firebaseConfig = {
+  apiKey: "AIzaSyABJVniFMAQjsU1GHNBHGJH6xl-AV7B9qs",
+  authDomain: "user-app-a74a8.firebaseapp.com",
+  databaseURL: "https://user-app-a74a8.firebaseio.com",
+  projectId: "user-app-a74a8",
+  storageBucket: "user-app-a74a8.appspot.com",
+  messagingSenderId: "425937931316",
+  appId: "1:425937931316:web:fbcd34b13e2cedc1858e61",
+};
 
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
